48ccab95659effadebab5a1ec0c760816b2533ca
Sudo-Byte-Me <twiseman@rincon.com>
2025-05-28 16:28:54 -0600

    update
    made SNR calc a function
    Added a gui practice file

--- Phys_Sim/up_down_link.py
+++ Phys_Sim/up_down_link.py
@@ -7,6 +7,16 @@ T = 290             # noise temperature
 k = 1.38*10**-23    # Boltzmann's constant in J/K
 c0 = 3e8            # speed of light
 
+#### calculations ####
+def compute_parameters(Gt, Pt, f, B, Gr, R):
+    lam = c0 / f                                                        # wavelength in m
+    Pn = k * T * B                                                      # noise power in W
+    Pr = 10**(Gt / 10) * 10**(Gr/10) * Pt * (lam / (4 * np.pi * R))**2  # received power
+    SNR = Pr / Pn                                                       # signal to noise ratio
+    SNR_dB = 10 * np.log10(SNR)                                         # convert to dB
+    H = B * np.log2(1 + SNR)                                            # channel capacity in bps
+    return lam, Pn, Pr, SNR_dB, H
+
 ######## uplink calculations ########
 #### adjustable parameters ####
 Gt = float(input("Primary station tx antenna gain (dB): "))             # antenna gain in dBi
@@ -16,13 +26,7 @@ B = float(input("Transmit signal bandwidth (Hz): "))                    # bandwi
 Gr = float(input("Repeater rx antenna gain (dB): "))                    # antenna gain in dBi
 R = float(input("Distance from primary station to repeater station: ")) # distance in m 
 
-#### calculations ####
-lam = c0 / f                                                        # wavelength in m
-Pn = k * T * B                                                      # noise pwower in W
-Pr = 10**(Gt / 10) * 10**(Gr/10) * Pt * (lam / (4 * np.pi * R))**2  # received power
-SNR = Pr / Pn                                                       # signal to noise ratio
-SNR_dB = 10 * np.log10(SNR)                                         # convert to dB
-H = B * np.log2(1 + SNR)                                            # channel capacity in bps
+lam, Pn, Pr, SNR_dB, H = compute_parameters(Gt, Pt, f, B, Gr, R)
 
 #### output ####
 print("Wavelength (m): ", lam)
@@ -40,14 +44,7 @@ f = float(input("Repeater new transmit frequency (Hz): "))                  # fr
 Gr = float(input("Secondary station rx antenna gain (dB): "))               # antenna gain in dBi
 R = float(input("Distance from repeater station to secondary station: "))   # distance in m 
 
-#### calculations ####
-lam = c0 / f                                                        # wavelength in m
-Pt = 10**(Amp_Gain/10) * Pr                                         # transmit power in W  
-Pn = k * T * B                                                      # noise pwower in W
-Pr = 10**(Gt / 10) * 10**(Gr/10) * Pt * (lam / (4 * np.pi * R))**2  # received power
-SNR = Pr / Pn                                                       # signal to noise ratio
-SNR_dB = 10 * np.log10(SNR)                                         # convert to dB
-H = B * np.log2(1 + SNR)                                            # channel capacity in bps
+lam, Pn, Pr, SNR_dB, H = compute_parameters(Gt, Pt, f, B, Gr, R)
 
 #### output ####
 print("Wavelength (m): ", lam)
