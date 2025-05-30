1669687c9cca0d5fe255f2047b2e3ac0e5dcea26
kdpriorRincon <kprior@rincon.com>
2025-05-30 17:05:11 -0600

    got a gui link working, but need to link it to main gui and add another image/button for command and control

--- Phys_Sim/main.py
+++ Phys_Sim/main.py
@@ -4,8 +4,13 @@ import Sig_Gen as Sig_Gen
 import Receiver as Receiver
 import Repeater as Repeater
 
-with ui.row():
-    simulate_button = ui.image('/media/simulate_low_opacity.png')
-    #command_button = ui.image('Phys_Sim/media/command_low_opacity.png', width='40%')
+
+#front page
+with ui.row().style('height: 100vh; width: 100%; display: flex; justify-content: center; align-items: center;'):
+    with ui.link(target='youtube.com'):
+        simulate_button = ui.image('media/simulate.png').style('width: 50vh; height: 50vh;')
+    #TODO: make this command and control the opacity of the image
+    #simulate_copy = ui.image('media/simulate_low_opacity.png').style('width: 50vh; height: 50vh;')
 
 ui.run()
+# with ui.row().style('justify-content: flex-start;'):
