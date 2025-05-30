4c44a94c7db5025740c2b96d11151d011f51a81d
kdpriorRincon <kprior@rincon.com>
2025-05-30 15:42:43 -0600

    moved main.py to Phys_Sim directory and updated imports

--- Phys_Sim/Receiver.py
+++ Phys_Sim/Receiver.py
@@ -3,5 +3,5 @@ class Receiver:
         self.sampling_rate = sampling_rate
         self.frequency = frequency
 
-    def decode_signal(self, <TODO>):
-        pass
+    # def decode_signal(self, <TODO>):
+    #     pass
--- main.py
+++
@@ -1,5 +0,0 @@
-# this file will be used to run the GUI
-from nicegui import ui
-from Phys_Sim import Sig_Gen as sig_gen
-from Phys_Sim import sim_qpsk_noisy_demod as qnd
-from Phys_Sim import sim_link_budget as lb
