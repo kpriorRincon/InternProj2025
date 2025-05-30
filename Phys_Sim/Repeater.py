b969cc475a5eb886c65245351b690d9007fd904d
kdpriorRincon <kprior@rincon.com>
2025-05-30 14:06:57 -0600

    modifications to repeater class file

--- Phys_Sim/Repeater.py
+++ Phys_Sim/Repeater.py
@@ -2,16 +2,38 @@ class Repeater:
     def __init__(self, desired_frequency):
         self.desired_freqeuncy = 1e9  # Default frequency set to 1 GHz
 
-    def mix(self, signal):
+    def mix(self, qpsk_signal, qpsk_frequency, t):
         """
         Mixes the input signal with a carrier frequency.
 
         Parameters:
         - signal: The input signal to be mixed.
+        - qpsk_frequency: The frequency of the QPSK signal.
+        - t: Time vector for the signal.
 
         Returns:
         - The mixed signal.
         """
         # Implement mixing logic here
         pass
+
+    def filter(self, cuttoff_frequency):
+        """
+        Filters the mixed signal to remove unwanted frequencies.
+
+        Returns:
+        - The filtered signal.
+        """
+        # Implement filtering logic here
+        pass
     
+    def amplify(self, gain, input_signal):
+        """
+        Amplifies the signal by a specified gain.
+        Parameters:
+        - gain: The gain factor to amplify the signal.
+        Returns:
+        - The amplified signal.
+        """
+        # Implement amplification logic here
+        return gain*input_signal
