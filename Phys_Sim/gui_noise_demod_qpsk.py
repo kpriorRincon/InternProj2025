432d76df5127f1816076e5a19eda6c157a4d1e95
Sudo-Byte-Me <twiseman@rincon.com>
2025-05-30 08:08:11 -0600

    name changes

--- Phys_Sim/gui_noise_demod_qpsk.py
+++ Phys_Sim/gui_noise_demod_qpsk.py
@@ -1,6 +1,6 @@
 from nicegui import ui
 import plotly.graph_objects as go
-import sim_qpsk_noisy_read as sqnr
+import Phys_Sim.sim_qpsk_noisy_demod as sqnr
 
 #################################################
 #
