6d008f6821f32fc10d1dc1475381d72d99677c0e
kdpriorRincon <kprior@rincon.com>
2025-05-30 14:19:11 -0600

    receiver class created

---
+++ Phys_Sim/Receiver.py
@@ -0,0 +1,7 @@
+class Receiver:
+    def __init__(self, sampling_rate, frequency):
+        self.position = position  # Position of the receiver in 3D space
+        self.orientation = orientation  # Orientation of the receiver in 3D space
+
+    def decode_signal(self, <TODO>):
+        pass
