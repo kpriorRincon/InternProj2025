63925d2a9247b573910ceeef59de67af6dd7da13
Sudo-Byte-Me <twiseman@rincon.com>
2025-05-30 15:37:30 -0600

    finished the filter function

--- Phys_Sim/experimental_files/filter_practice.py
+++ Phys_Sim/experimental_files/filter_practice.py
@@ -35,24 +35,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-f_cutoff = 930e6
-f_sample = 10 * f_cutoff  # 9.3 GHz
-order = 5
+def filter_the_signal(f_cutoff, f_lo, f_in, order=5):
+    # Sampling frequency
+    f_sample = 10 * f_cutoff    
 
-# Time vector
-duration = 5 / 1.83e9  # 5 cycles of f_lo
-t = np.arange(0, duration, 1 / f_sample)
+    # Time vector
+    duration = 20 / f_lo                      # number of cycles of f_lo
+    t = np.arange(0, duration, 1 / f_sample)    # time vector
 
-# Mixed signal
-f_lo = 1.83e9
-f_in = 910e6
-sig = np.cos(2 * np.pi * f_in * t) * np.cos(2 * np.pi * f_lo * t)
+    # Mixed signal
+    sig = np.cos(2 * np.pi * f_in * t) * np.cos(2 * np.pi * f_lo * t)   # Mixed Signal
 
-# Design Butterworth LPF
-b, a = signal.butter(order, f_cutoff, btype='low', fs=f_sample)
+    # Design Butterworth LPF
+    b, a = signal.butter(order, f_cutoff, btype='low', fs=f_sample) # butterworth filter coefficients
 
-# Apply filter
-filtered_sig = signal.filtfilt(b, a, sig)
+    # Apply filter
+    filtered_sig = signal.filtfilt(b, a, sig)   # filtered signal
+
+    return t, sig, filtered_sig
+
+##### Test the Function #####
+f_cutoff = 930e6            # Cutoff frequency
+order = 5                   # Filter order
+f_lo = 1.83e9   # Local Oscillator
+f_in = 910e6    # Input frequency
+
+t, sig, filtered_sig = filter_the_signal(f_cutoff, f_lo, f_in, order)
 
 # Plot
 plt.figure()
