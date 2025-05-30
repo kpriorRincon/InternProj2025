1ba1971b82f9e962ccce57181318289575e7df72
kdpriorRincon <kprior@rincon.com>
2025-05-30 13:58:52 -0600

    created repeater class for Phys_Sim

---
+++ Phys_Sim/Repeater.py
@@ -0,0 +1,17 @@
+class Repeater:
+    def __init__(self, desired_frequency):
+        self.desired_freqeuncy = 1e9  # Default frequency set to 1 GHz
+
+    def mix(self, signal):
+        """
+        Mixes the input signal with a carrier frequency.
+
+        Parameters:
+        - signal: The input signal to be mixed.
+
+        Returns:
+        - The mixed signal.
+        """
+        # Implement mixing logic here
+        pass
+    
