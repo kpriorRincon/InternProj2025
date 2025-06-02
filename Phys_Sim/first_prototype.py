ac2227fc86cc66e4f90e50b30fe415f26622aaeb
JHernandezRincon <jhernandez@rincon.com>
2025-06-02 11:33:42 -0600

    update on repeater

--- Phys_Sim/first_prototype.py
+++ Phys_Sim/first_prototype.py
@@ -3,9 +3,19 @@ import Receiver as Receiver
 import Repeater as Repeater
 
 #create all of the objects
-sig_gen = Sig_Gen.SigGen()
+
 receiver = Receiver.Receiver(sampling_rate=1e6, frequency=915e6)
 repeater = Repeater.Repeater(sampling_rate=1e6, frequency=915e6)
 user_input = input("Enter a message to be sent: ")
 message_bits = sig_gen.set_message(user_input)
 sig_gen.generate_qpsk()
+
+def main():
+    sig_gen = Sig_Gen.SigGen()
+    user_input = input("Enter a message to be sent: ")
+    message_bits = sig_gen.message_to_bits(user_input)
+
+    #t, qpsk, lines, symbols = 
+
+if __name__ == "__main__":
+    main()
