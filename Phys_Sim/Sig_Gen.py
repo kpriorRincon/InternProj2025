41a9dedb87490582d0a031df861d5c63031c5360
kdpriorRincon <kprior@rincon.com>
2025-06-04 12:35:34 -0600

    changes in main

--- Phys_Sim/Sig_Gen.py
+++ Phys_Sim/Sig_Gen.py
@@ -91,3 +91,90 @@ class SigGen:
         # Convert string input to list of integers
         bit_sequence = [int(bit) for bit in message_binary.strip()]
         return bit_sequence
+
+    def plot_time_png(self,message):
+        "this function plots 3 sections of the qpsk wave form in the time domain"
+        " and stores them into png files in the qpsk_sig_gen folder'"
+        import matplotlib.pyplot as plt
+        import numpy as np
+        plt.figure(figsize=(15, 5))
+        plt.plot(self.time_vector, self.qpsk_waveform)
+        plt.ylim(-1/np.sqrt(2)*self.amp-.5, 1/np.sqrt(2)*self.amp+.5)
+
+        #if there are more than 10 symbols only show the first ten symbols
+        if len(self.time_vertical_lines) > 10:
+            plt.xlim(0, 10/self.symbol_rate)  # Show first 10 symbol periods
+        #if not don't touch the xlim
+
+        for lines in self.time_vertical_lines:
+            #add vertical lines at the symbol boundaries
+            if len(self.time_vertical_lines) > 10:
+                if lines < 9/self.symbol_rate:
+                    plt.axvline(x=lines, color='black', linestyle='--', linewidth=1)
+
+                    #add annotation for the symbol e.g. '00', '01', '10', '11'
+                    # Reverse mapping: symbol -> binary pair
+                    symbol = self.symbols[self.time_vertical_lines.index(lines)]
+                    # Reverse the mapping to get binary pair from symbol
+                    reverse_mapping = {v: k for k, v in self.mapping.items()}
+                    binary_pair = reverse_mapping.get(symbol, '')
+                    formatted_pair =str(binary_pair).replace("(", "").replace(")", "").replace(", ", "")
+                    #debug
+                    #print(formatted_pair)
+                    x_dist = 1 / (2.7 * self.symbol_rate) #half the symbol period 
+                    y_dist = 0.707*self.amp + .2 # 0.807 is the amplitude of the QPSK waveform
+                    plt.annotate(formatted_pair, xy=(lines, 0), xytext=(lines + x_dist, y_dist), fontsize=17)  
+            else:
+                if lines < len(self.time_vector):
+                    plt.axvline(x=lines, color='black', linestyle='--', linewidth=1)
+
+                    #add annotation for the symbol e.g. '00', '01', '10', '11'
+                    # Reverse mapping: symbol -> binary pair
+                    symbol = self.symbols[self.time_vertical_lines.index(lines)]
+                    # Reverse the mapping to get binary pair from symbol
+                    reverse_mapping = {v: k for k, v in self.mapping.items()}
+                    binary_pair = reverse_mapping.get(symbol, '')
+                    formatted_pair =str(binary_pair).replace("(", "").replace(")", "").replace(", ", "")
+                    #debug
+                    #print(formatted_pair)
+                    x_dist = 1 / (2.7 * self.symbol_rate) #half the symbol period 
+                    y_dist = 0.707*self.amp + .2 # 0.807 is the amplitude of the QPSK waveform
+                    plt.annotate(formatted_pair, xy=(lines, 0), xytext=(lines + x_dist, y_dist), fontsize=17)
+                
+        if len(self.time_vertical_lines) > 10:
+            plt.title(f'QPSK Waveform for \"{message}\" (first 10 symbol periods)')
+        else:
+            plt.title(f'QPSK Waveform for \"{message}\"')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        plt.grid()
+        # Save the plot to a file
+        plt.savefig(f'qpsk_sig_gen/1_qpsk_waveform.png', dpi=300)    
+        #print("Debug: plot generated")
+        return
+    
+    def plot_freq_png(self):
+        #same figure size as above
+        import matplotlib.pyplot as plt
+        import numpy as np
+        n = len(self.time_vector)
+        freqs = np.fft.fftfreq(n, d=1/self.sample_rate)
+        # FFT of original and shifted signals
+        fft = np.fft.fft(self.qpsk_waveform)
+        fft_db = 20 * np.log10(np.abs(fft))
+        # get fft of qpsk signal
+        
+        plt.figure(figsize=(15, 5))
+        plt.plot(freqs,fft_db)
+        plt.title("FFT of QPSK signal")
+        plt.xlim(0, 1000e6)
+        plt.ylim(0, np.max(fft_db)+10)
+        #save plot
+        plt.savefig(f'qpsk_sig_gen/2_qpsk_waveform.png', dpi = 300)
+
+    def handler(self,message, frequency):
+        self.freq = frequency
+        self.symbol_rate = .01*frequency
+        self.generate_qpsk(self.message_to_bits(message))
+        self.plot_time_png(message)
+        self.plot_freq_png()
