4811282a04810052afad5ba986a2e6b3c7a58698
kdpriorRincon <kprior@rincon.com>
2025-06-16 14:35:42 -0600

    adding most recent changes to viewer.py

--- Cesium_Sattelite/viewer.py
+++ Cesium_Sattelite/viewer.py
@@ -93,7 +93,7 @@ def submit():
     # convert tle_list to czml
 
     # Convert to CZML
-    czml_obj = satellite_czml(tles)  # this should create a new object
+    czml_obj = satellite_czml(tles)  # this should create a new object and with the modifications at the top of the program they each instance will have it's own dictionary. 
     czml_string = czml_obj.get_czml()
     # testing if the string is gettin appended to or not
     print(len(czml_string))
@@ -133,11 +133,10 @@ def Cesium_page():
     ts = load.timescale()
     now_utc = datetime.now(timezone.utc)
     start_time = ts.utc(now_utc)  # convert to skyfield time
-    # do in 3 hour steps
-    # Increase frequency: check every 5 minutes (0.0833 hours)
-    # Check every minute for 2 days: 2 days * 24 hours * 60 minutes = 2880 steps
-    time_range = [start_time + timedelta(minutes=i)
-                  for i in range(2 * 24 * 60)]
+
+    # Check every minute for 1 day: 1 day * 24 hours * 60 minutes = 1440 steps
+    time_range = [start_time + timedelta(minutes=i) for i in range(1 * 24 * 60)]
+
     # Track unique satellite crossings, only keep minimum distance per satellite we can do the number of satellites the user selected as the cap
     crossings = {}
 
@@ -163,7 +162,7 @@ def Cesium_page():
         if len(crossings) >= count:
             break
     with ui.column().style('width: 25%'):
-        ui.label('Closest Satellite Crossings (1 minute increments for 2 days)').style(
+        ui.label('Closest Satellite Crossings (1 minute increments for 1 day)').style(
             'font-size: 1.5em; font-weight: bold; margin-top: 1em; margin-bottom: 0.5em; white-space: normal; word-break: break-word;')
 
 
@@ -227,9 +226,25 @@ def Cesium_page():
         
         # Convert tx_pos (wgs84.latlon) to ECEF meters at the crossing time
         tx_ecef = tx_pos.at(time_crossing_skyfield).position.m
+        rx_ecef = rx_pos.at(time_crossing_skyfield).position.m
         # Unit vector from transmitter to satellite
-        k_tr = (position_vector - tx_ecef) / np.linalg.norm(position_vector - tx_ecef)#unit vector from transmitter to receiver 
-
+        k_ts = (position_vector - tx_ecef) / np.linalg.norm(position_vector - tx_ecef)#unit vector from transmitter to satellite
+        k_sr = (position_vector - rx_ecef) / np.linalg.norm(rx_ecef - position_vector)#unit vector from satellite to receiver
+        f_c = 905e6
+        f_doppler_shifted = (f_c-np.dot(velocity_vector, k_ts)) # since stationary transmitter 
+        ui.label(f'The doppler shifted frequency for uplink: {f_doppler_shifted} Hz')
+        
         #downlink doppler 
+        ui.label("we assume that the repeater transmits at 915 MHz")
+        f_c = 915e6
+        f_doppler_shifted = (f_c*f_c)/(f_c-np.dot(velocity_vector, k_sr))
+        ui.label(f'The doppler shifted frequency for downlink: {f_doppler_shifted} Hz')
 
+
+        #time delay up and down:
+        c = 2.99792458e8
+        time_delay_up = np.linalg.norm(position_vector-tx_ecef) / c # m/m/s = s
+        ui.label(f'time delay up {time_delay_up} s')
+        time_delay_down = np.linalg.norm(rx_ecef - position_vector) / c 
+        ui.label(f'time delay down {time_delay_down} s')
 ui.run()
