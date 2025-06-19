6ac0fe4c65879afe056b6689f8a0b77281890a92
kdpriorRincon <kprior@rincon.com>
2025-06-19 08:29:17 -0600

    adding some of advanced sim items like sig gen and channel up

--- Cesium_Sattelite/Sig_Gen.py
+++ Cesium_Sattelite/Sig_Gen.py
@@ -1,6 +1,6 @@
 class SigGen:
 
-    def __init__(self, freq=1.0, amp=1.0, sample_rate=100000, symbol_rate=1000):
+    def __init__(self, freq=1.0, amp=1.0, sample_rate=40e6, symbol_rate=4e6):
         import numpy as np
         self.freq = freq  # Frequency in Hz
         self.sample_rate = sample_rate  # sample rate in samples per second
@@ -67,7 +67,7 @@ class SigGen:
 
         return time, h
 
-    def generate_qpsk(self, bits, bool_noise, noise_power=0.01):
+    def generate_qpsk(self, bits):
         """
         Generate a QPSK signal from a sequence of bits.
 
@@ -101,39 +101,22 @@ class SigGen:
         t = np.arange(total_samples) / self.sample_rate
 
         # Upsample symbols to match sampling rate
-        # Each symbol is held constant for samples_per_symbol duration
-        upsampled_symbols = np.concatenate(
-            [np.append(x, np.zeros(samples_per_symbol-1, dtype=complex))for x in symbols])
+        #this will make an array like [(1+1j), 0, 0, 0, 0, 0, 0, 0,..., (1-1j), ]
+        upsampled_symbols = np.concatenate([np.append(x, np.zeros(samples_per_symbol-1, dtype=complex))for x in symbols])
 
         # Root raised cosine filter implementation
-        from commpy import filters
         beta = 0.3
-        _, pulse_shape = self.rrc_filter(
-            beta, 300, 1/self.symbol_rate, self.sample_rate)
+        _, pulse_shape = self.rrc_filter(beta, 300, 1/self.symbol_rate, self.sample_rate)
+        
         # pulse_shape = np.convolve(pulse_shape, pulse_shape)/2
         signal = np.convolve(pulse_shape, upsampled_symbols, 'same')
 
         # Generate complex phasor at carrier frequency
         phasor = np.exp(1j * 2 * np.pi * self.freq * t)
 
-        # Modulate: multiply upsampled symbols by phasor
+        # Modulate: multiply pulse shaped upsampled symbols with the complex carrier
         qpsk_waveform = signal * phasor
-
-        # get vertical lines
-        t_vertical_lines = []  # Initialize vertical lines for debugging
-        for i, symbol in enumerate(symbols):
-            # compute the phase offset for the symbol
-            phase_offset = np.angle(symbol)
-            # debugging print statement to show the symbol and phase offset
-            # print(f"Symbol {i}: {symbol}, Phase Offset: {phase_offset}");
-            idx_start = i * samples_per_symbol
-            t_vertical_lines.append(idx_start/self.sample_rate)
-
-        if bool_noise:
-            # add noise to the QPSK wavefrorm
-            qpsk_waveform = self.noise_adder(qpsk_waveform, noise_power)
-
-        return t, qpsk_waveform, symbols, t_vertical_lines, upsampled_symbols
+        return t, qpsk_waveform
 
     def message_to_bits(self, message):
         """
