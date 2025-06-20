2928b259b3d186b3ee0540dd2409ab99b0863414
Montuck <trevor_wiseman@outlook.com>
2025-06-20 08:36:15 -0600

    updated manual_processing

---
+++ GNU_Radio/testing/manual_processing/correlator.py
@@ -0,0 +1,46 @@
+import numpy as np
+from scipy.signal import fftconvolve
+import modulator as md
+
+# Define start and end sequences
+# 1 1 1 1 1 0 0 1 1 0 1 0 0 1 0 0 0 0 1 0 1 0 1 1 1 0 1 1 0 0 0 1
+start_sequence = [1, 1, 1, 1, 1, 0, 0, 1,
+                1, 0, 1, 0, 0, 1, 0, 0,
+                0, 0, 1, 0, 1, 0, 1, 1,
+                1, 0, 1, 1, 0, 0, 0, 1]
+
+# 0 0 1 0 0 1 1 0 1 0 0 0 0 0 1 0 0 0 1 1 1 1 0 1 0 0 0 1 0 0 1 0
+end_sequence = [0, 0, 1, 0, 0, 1, 1, 0,
+                1, 0, 0, 0, 0, 0, 1, 0, 
+                0, 0, 1, 1, 1, 1, 0, 1, 
+                0, 0, 0, 1, 0, 0, 1, 0]
+
+start_symbols = md.generate_qpsk(start_sequence)
+end_symbols = md.generate_qpsk(end_sequence)
+
+def correlator(rx_signal, sps):
+    """
+    Correlates the received signal with a known
+    start and end sequence to detect the desired signal
+    
+    Parameters:
+        rx_signal:          received complex signal array
+        known_signal:       known complex signal array
+        correlated signal:  complex array resulting
+                            from the correlation
+    
+    Author: Trevor Wiseman
+    """
+
+    start_index = 0
+    end_index = len(rx_signal) - 1
+      
+    # Correlate with start sequence
+    correlated_signal = fftconvolve(rx_signal, np.conj(np.flip(start_symbols)), mode='full')
+    end_cor_signal = fftconvolve(rx_signal, np.conj(np.flip(end_symbols)), mode='full')
+    
+    # Find maximum correlation
+    start_index = np.argmax(np.abs(correlated_signal)) - 16*sps # go back 16 symbols e.g. 32 bits
+    end_index = np.argmax(np.abs(end_cor_signal))
+    
+    return start_index, end_index
