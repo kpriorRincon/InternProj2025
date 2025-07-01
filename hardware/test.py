05fffd0ad78f1be4ce708dfe84e3699497debbbd
rinterns25 <rinterns25@gmail.com>
2025-07-01 12:41:00 -0600

    send just the marker

--- hardware/test.py
+++ hardware/test.py
@@ -1,5 +1,6 @@
 import transmit_processing as transmit_processing
 import receive_processing as receive_processing 
+import numpy as np
 
 message = input('Enter your message: \n')
 sps = 4
@@ -7,16 +8,23 @@ sample_rate = 2.88e6
 beta = 0.35
 N = 40
 
-transmit_processing = transmit_processing.transmit_processing(sps, sample_rate)
-receive_processing = receive_processing.receive_processing(sps, sample_rate)
+tp = transmit_processing.transmit_processing(sps, sample_rate)
+rp = receive_processing.receive_processing(sps, sample_rate)
 
-bits_out, data = transmit_processing.work(message, beta, N)
+#bits_out, data = transmit_processing.work(message, beta, N)
 
-bits_in, decoded_message = receive_processing.work(data, beta, N)
+#bits_in, decoded_message = receive_processing.work(data, beta, N)
 
+#print("Message Sent: ", message)
+#print("Message Received: ", decoded_message)
+#print("Bits Sent: ", bits_out)
+#print("Bits Received: ", bits_in)
 
-print("Message Sent: ", message)
-print("Message Received: ", decoded_message)
-print("Bits Sent: ", bits_out)
-print("Bits Received: ", bits_in)
+start_marker, end_marker = tp.generate_markers()
+start_data, end_data = tp.modulated_markers(beta, N, start_marker, end_marker)
+
+zeros = np.zeros(len(start_data)*3, dtype=np.complex64)
+data = np.append(start_data, zeros)
+
+data.tofile("data_for_sighound.bin")
 
