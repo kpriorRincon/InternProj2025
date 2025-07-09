2929a639e86ee0258b89b6c6ba2fddab656ddc92
Sudo-Byte-Me <twiseman@rincon.com>
2025-07-09 11:47:14 -0600

    added timer to crc and debug statements

--- hardware/CRC_Testing/CRC_Input.py
+++ hardware/CRC_Testing/CRC_Input.py
@@ -1,11 +1,12 @@
-import hardware.CRC_Testing.CRC_Transmit_Processing as CRC_Transmit_Processing
-import receive_processing as receive_processing 
+import CRC_Transmit_Processing as CRC_Transmit_Processing
+import CRC_Receive_Processing as CRC_Receive_Processing 
 import numpy as np
 from config import *
 from crc import Calculator, Crc8
+import time
 
 # initialize the CRC calculator
-calculator = Calculator(Crc8.CCITT)
+calculator = Calculator(Crc8.CCITT, optimized=True)
 
 # get user input
 message = input('Enter your message: \n')
@@ -18,16 +19,19 @@ N = NUMTAPS
 
 # call tx and rx objects
 tp = CRC_Transmit_Processing.transmit_processing(sps, sample_rate)
-rp = receive_processing.receive_processing(sps, sample_rate)
+rp = CRC_Receive_Processing.receive_processing(sps, sample_rate)
 
 # convert message to bits for transmission
 bits_out, data = tp.work(message, beta, N)
+print("Data to send: ", bits_out)
 
 # decode the received data
 bits_in, decoded_message, symbols = rp.work(data, beta, N)
 
 # error check
+timer = time.time()
 check = calculator.checksum(data)
+print("Time to run CRC check: ", time.time() - timer)
 print("Remainder: ", check)
 if check == 0:
     print("Data is valid...")
@@ -37,13 +41,5 @@ if check == 0:
     print("Bits Sent: ", bits_out)
     print("Bits Received: ", bits_in)
 else:
+    print("Message Received: ", decoded_message)
     print("Data is invalid...\nAborting...")
-
-#start_marker, end_marker = tp.generate_markers()
-#start_data, end_data = tp.modulated_markers(beta, N, start_marker, end_marker)
-
-#zeros = np.zeros(len(start_data)*3, dtype=np.complex64)
-#data = np.append(start_data, zeros)
-
-#data.tofile("data_for_sighound.bin")
-
