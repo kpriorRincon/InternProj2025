b8eabf37553caf501b386498183ff3efd570ebd8
kdpriorRincon <kprior@rincon.com>
2025-07-15 15:01:01 -0600

    pushing new file

---
+++ Live_Audio/Tx.py
@@ -0,0 +1,25 @@
+"""
+===============================================================================
+File Name     : Tx.py
+Description   : collect audio samples modulate them using 16-bit QAM and transmit them
+Authors       : Skylar Harris, Jorge Hernandez, Kobe Prior, Trevor Wiseman=
+Created       : 2025-07-15
+Last Modified : 2025-07-18
+Version       : 1.0
+Python Version: 3.x
+===============================================================================
+
+Notes:
+- Any additional context or usage instructions.
+- External dependencies (e.g., numpy, matplotlib).
+- Reference links or documentation sources if applicable.
+
+Example Usage:
+    $ python Tx.py
+
+===============================================================================
+"""
+
+# Library Imports
+import os
+import sys
