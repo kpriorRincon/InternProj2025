fa7ea9da27cc48c299d8fc6ec02c962a69a94b4e
kdpriorRincon <kprior@rincon.com>
2025-07-16 09:30:52 -0600

    pushin changes

--- Live_Audio/decode_16qam.py
+++ Live_Audio/decode_16qam.py
@@ -8,13 +8,19 @@ def normalize_16QAM(signal):
     avg_power = np.mean(np.abs(signal)**2)
     normalized_signal = signal/avg_power
     return normalized_signal
+def decide(ready_sig):
 
-def demodulate(ready_sig):
     #here we need to create a decision rules
+    #normalized levels
     a = np.array([-3,-1,1,3])/np.sqrt(10)
-    boundaries = {
-        'real':[(a[i] + a[i+1])/2 for i in range(len(a)-1)],
-        'imag':[(a[i] + a[i+1])/2 for i in range(len(a)-1)]
-    }
+    def quantize(value):
+        return a[np.argmin(np.abs(value - a))]
+    #decisions = [(1+1j)/root(10), ...., (-1 -3j)/root(10)] essentially snaps symbols to the closest constellation
+    decisions = [(quantize(np.real(symbol)) + 1j * quantize(np.imag(symbol)) for symbol in ready_sig)]
+    return decisions
+
+def demodulate(decisions):
+    #we need to get our decisions into bits
+    bits = 
     
     return bits
