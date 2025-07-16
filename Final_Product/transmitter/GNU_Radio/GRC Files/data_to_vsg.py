3066bbc22be8e6eaf91a0147ef1f819470c4a704
rinterns25 <rinterns25@gmail.com>
2025-07-16 15:06:50 -0600

    changed config for rtl

--- Final_Product/transmitter/GNU_Radio/GRC Files/data_to_vsg.py
+++ Final_Product/transmitter/GNU_Radio/GRC Files/data_to_vsg.py
@@ -15,11 +15,9 @@ from gnuradio.filter import firdes
 from gnuradio.fft import window
 import sys
 import signal
-import time
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio import soapy
 import vsg60
 
 
@@ -41,33 +39,7 @@ class data_to_vsg(gr.top_block):
         # Blocks
         ##################################################
         self.vsg60_iqin_0 = vsg60.iqin(freq_tx, 0, samp_rate, False)
-        self.soapy_bladerf_source_0 = None
-        dev = 'driver=bladerf'
-        stream_args = ''
-        tune_args = ['']
-        settings = ['']
-
-        self.soapy_bladerf_source_0 = soapy.source(dev, "fc32", 1, '',
-                                  stream_args, tune_args, settings)
-        self.soapy_bladerf_source_0.set_sample_rate(0, samp_rate)
-        self.soapy_bladerf_source_0.set_bandwidth(0, 0.0)
-        self.soapy_bladerf_source_0.set_frequency(0, freq_tx)
-        self.soapy_bladerf_source_0.set_frequency_correction(0, 0)
-        self.soapy_bladerf_source_0.set_gain(0, min(max(30.0, -1.0), 60.0))
-        self.soapy_bladerf_sink_0 = None
-        dev = 'driver=bladerf'
-        stream_args = ''
-        tune_args = ['']
-        settings = ['']
-
-        self.soapy_bladerf_sink_0 = soapy.sink(dev, "fc32", 1, '',
-                                  stream_args, tune_args, settings)
-        self.soapy_bladerf_sink_0.set_sample_rate(0, samp_rate)
-        self.soapy_bladerf_sink_0.set_bandwidth(0, 0.0)
-        self.soapy_bladerf_sink_0.set_frequency(0, freq_rx)
-        self.soapy_bladerf_sink_0.set_frequency_correction(0, 0)
-        self.soapy_bladerf_sink_0.set_gain(0, min(max(40, 17.0), 73.0))
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/empire/Documents/InternProj2025/Final_Product/transmitter/data_for_sighound.bin', True, 0, 0)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/empire/Documents/InternProj2025/Experimentation/Error_Correction/RSCorrect_Testing/data_for_sighound.bin', True, 0, 0)
         self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
 
 
@@ -75,7 +47,6 @@ class data_to_vsg(gr.top_block):
         # Connections
         ##################################################
         self.connect((self.blocks_file_source_0, 0), (self.vsg60_iqin_0, 0))
-        self.connect((self.soapy_bladerf_source_0, 0), (self.soapy_bladerf_sink_0, 0))
 
 
     def get_samp_rate(self):
@@ -83,8 +54,6 @@ class data_to_vsg(gr.top_block):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.soapy_bladerf_sink_0.set_sample_rate(0, self.samp_rate)
-        self.soapy_bladerf_source_0.set_sample_rate(0, self.samp_rate)
         self.vsg60_iqin_0.set_srate(self.samp_rate)
 
     def get_freq_tx(self):
@@ -92,7 +61,6 @@ class data_to_vsg(gr.top_block):
 
     def set_freq_tx(self, freq_tx):
         self.freq_tx = freq_tx
-        self.soapy_bladerf_source_0.set_frequency(0, self.freq_tx)
         self.vsg60_iqin_0.set_frequency(self.freq_tx)
 
     def get_freq_rx(self):
@@ -100,7 +68,6 @@ class data_to_vsg(gr.top_block):
 
     def set_freq_rx(self, freq_rx):
         self.freq_rx = freq_rx
-        self.soapy_bladerf_sink_0.set_frequency(0, self.freq_rx)
 
 
 
@@ -119,10 +86,6 @@ def main(top_block_cls=data_to_vsg, options=None):
 
     tb.start()
 
-    #run for the fixed duration
-    run_duration = 3 # in seconds
-    time.sleep(run_duration)
-    tb.stop()
     tb.wait()
 
 
