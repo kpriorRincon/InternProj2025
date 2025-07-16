56527fcd3e28f68622cbf98b25ca4cb9166b2870
kdpriorRincon <kprior@rincon.com>
2025-07-16 11:19:37 -0600

    pushing all new changes

--- Live_Audio/decode_16qam.py
+++ Live_Audio/decode_16qam.py
@@ -7,11 +7,11 @@ def decimate(signal):
 def normalize_16QAM(signal):
     #plots are for debug
 
-    plt.plot(np.real(signal), np.imag(signal), 'o')
+    # plt.plot(np.real(signal), np.imag(signal), 'o')
     avg_power = np.mean(np.abs(signal) ** 2)
     normalized_signal = signal / avg_power
-    plt.plot(np.real(normalized_signal), np.imag(normalized_signal), 'o')
-    plt.show()
+    # plt.plot(np.real(normalized_signal), np.imag(normalized_signal), 'o')
+    # plt.show()
     return normalized_signal
 
 def decide(ready_sig):
--- Live_Audio/testing_demod.py
+++ Live_Audio/testing_demod.py
@@ -5,28 +5,38 @@ from scipy.signal import fftconvolve
 import scipy.io.wavfile
 import matplotlib.pyplot as plt
 import wave as wv
-CHUNK = 1024**2
-with wv.open('output.wav', 'rb') as wave_file:
-    d = wave_file.readframes(CHUNK)
 
-d_bits = np.unpackbits(np.frombuffer(d, dtype = np.uint8))
-print(d_bits)
-#limiting the amplitude so we can test quantization
+CHUNK = 1024
+sample_rate = 44100
 sig_gen = SigGen.SigGen(910e6, 1)
-#get the bits from wav file
-t, qam_sig = sig_gen.generate_16QAM(d_bits)
 
+reconstructed_bytes = bytearray()
 
-#tune that shit to baseband
-baseband_signal = qam_sig * np.exp(-1j * 910e6 * 2 * np.pi * t)
-rrc_added = fftconvolve(baseband_signal, sig_gen.h, mode = 'same')
+with wv.open('output.wav', 'rb') as wave_file:
+    while True:
+        d = wave_file.readframes(CHUNK)
+        if not d:
+            break
 
-bits_out = signal_to_bits(rrc_added)
-print(f'bits out {bits_out}')
+        # Convert to bits
+        d_bits = np.unpackbits(np.frombuffer(d, dtype=np.uint8))
 
-#reconstruct the bits into a wave file
-byte_array_out = np.packbits(bits_out)
-raw_bytes = byte_array_out.tobytes()
-samples = np.frombuffer(raw_bytes, dtype='<i2')
-sample_rate = 44100  # or use the original sample rate
+        # Modulate
+        t, qam_sig = sig_gen.generate_16QAM(d_bits)
+
+        # Mix to baseband
+        baseband_signal = qam_sig * np.exp(-1j * 910e6 * 2 * np.pi * t)
+        rrc_added = fftconvolve(baseband_signal, sig_gen.h, mode='same')
+
+        # Demodulate
+        bits_out = signal_to_bits(rrc_added)
+
+        # Pack bits into bytes
+        byte_array_out = np.packbits(bits_out)
+        reconstructed_bytes.extend(byte_array_out.tobytes())
+
+# Convert accumulated bytes to int16 samples
+samples = np.frombuffer(reconstructed_bytes, dtype='<i2')
+
+# Write to output WAV file
 scipy.io.wavfile.write('reconstructed.wav', sample_rate, samples)
